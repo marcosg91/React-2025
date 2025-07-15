061374be761723ef3f2713aa9ee44b1cbd8b7a00
marcosg91 <marcos_g91@hotmail.com>
2025-07-14 22:57:08 -0300

    modificacion player

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-import { PlayerProvider } from "./context/PlayerContext"; 
+import { PlayerProvider } from "./context/PlayerContext";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Router>
-      <PlayerProvider>
+    <PlayerProvider>
+      <Router>
         <App />
-      </PlayerProvider>
-    </Router>
+      </Router>
+    </PlayerProvider>
   </StrictMode>
 );
